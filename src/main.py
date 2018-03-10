08fa706e29f718ede767c58606af2e456f809861
Orlando hernandez <oaraz02@gmail.com>
2018-03-09 21:09:08 -0500

    new structure with frames

--- src/main.py
+++ src/main.py
@@ -1,56 +1,99 @@
-from tkinter import Button, Tk, Listbox, Frame
-from src.utils.utils import list_files
+import tkinter as tk
+from tkinter import filedialog
+import glob
 
-class PdfToAudio:
-    def __init__(self, master):
-        # ventana principal
-        self.master = master
-        # medida ventana principal
-        self.master.geometry("430x200")
-        # nombre ventana principal
-        self.master.title("PDF to Audio")
-        # la ventana no puede cambiar de tamaño
-        self.master.resizable(0, 0)
+LARGE_FONT = ("Verdana", 12)
 
-        # lista de archivos ya convertidos
-        self.listbox = Listbox(self.master, width=59, height=5)
-        # ubicacion dentro de la ventana de la lista de archivos
-        self.listbox.place(x=5, y=70)
 
-        # boton para realizar una nueva conversion
-        self.newButton = Button(self.master, text="NUEVO", command=self.new_conversion)
-        self.newButton.place(x=180, y=30)
+class PdfToAudio(tk.Tk):
 
-        # boton para realizar una nueva conversion
-        self.openButton= Button(self.master, text="ABRIR", command=self.show_selection)
-        self.openButton.place(x=180, y=150)
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
 
-        # muestra los libros o el material ya convetido
-        self.showAudios = self.show_audios()
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
+        self.frames = {}
+
+        for F in (StartPage, PageOne, PageTwo):
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        button = tk.Button(self, text="CONVERTIR TEXTO",
+                           command=lambda: controller.show_frame(PageOne))
+        button.pack(pady=10)
+
+        label = tk.Label(self, text="AUDIOS EXISTENTES", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        self.listbox = tk.Listbox(self, width=59, height=5)
+        self.listbox.pack(pady=5, padx=70)
+        self.show_audios()
+
+        button2 = tk.Button(self, text="ABRIR AUDIO",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack(pady=10)
 
     def show_audios(self):
-        listAudios = list_files("/home/ar4z/Audiolibros/*.mp3")
+        listAudios = glob.glob("/home/ar4z/Audiolibros/*.mp3")
         for audio, n_audio in zip(listAudios, range(len(listAudios))):
-            print("sw",audio, n_audio)
+            print("sw", audio, n_audio)
             self.listbox.insert(n_audio, audio)
 
 
-    def show_selection(self):
-        print(self.listbox.curselection())
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        button1 = tk.Button(self, text="EXAMINAR",
+                            command=self.select_pdf)
+        button1.pack(pady=10)
+        label = tk.Label(self, text="ARCHIVO: ", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        self.path_selected_file = tk.StringVar(None)
+        self.field_path_selected_file = tk.Entry(self, width='65', textvariable=self.path_selected_file).pack()
+        button2 = tk.Button(self, text="CONVERTIR",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack(pady=50)
+
+    def select_pdf(self):
+        selected_file = filedialog.askopenfilename(initialdir="/home/ar4z", title="SELECCIONAR LIBRO", filetypes=(("archivos pdf","*.pdf"),("todos los archivos","*.*")))
+        self.path_selected_file.set(selected_file)
 
 
-    def new_conversion(self):
-        
-        frame = Frame(self.master)
-        frame.pack()
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Page One",
+                            command=lambda: controller.show_frame(PageOne))
+        button2.pack()
 
 
-
-
-
-
-root = Tk()
-app = PdfToAudio(root)
-
-root.mainloop()
+app = PdfToAudio()
+app.mainloop()
